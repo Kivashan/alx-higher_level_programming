bedf09ba712165375a0970b7dae30acb31676a76
Kivashan <bvkivashan1982@gmail.com>
2023-05-08 16:51:18 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * check_cycle - checks if linked list is looped
+ * @list: start position of linked list
+ *
+ * Return: 1 (list is looped), otherwise 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp;
+
+	if (!list && (list->next != NULL))
+		return (0);
+
+	temp = list->next;
+
+	while (temp->next != NULL)
+	{
+		if (temp->n == list->n)
+			return (1);
+
+		temp = temp->next;
+	}
+
+	return (0);
+}
