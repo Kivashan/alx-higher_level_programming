da5e0a5b9dac5cdcb297e127786065dde3b33b6b
Kivashan <bvkivashan1982@gmail.com>
2023-05-11 20:41:03 -0700

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    temp = dir(hidden_4)
+
+    for i in temp:
+        if (i[0] == '_'):
+            continue
+        print("{}".format(i))
