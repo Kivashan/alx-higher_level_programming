4fbcc479dfb83952046aae27333dec633a839837
Kivashan <bvkivashan1982@gmail.com>
2023-05-15 14:42:36 -0700

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    length = len(my_list)
+    new_list = []
+
+    if (idx < 0) or (idx >= length):
+        return (my_list)
+
+    for i in range(0, length):
+        if (idx == i):
+            new_list.append(element)
+        else:
+            new_list.append(my_list[i])
+    return (new_list)
