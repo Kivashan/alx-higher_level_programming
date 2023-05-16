f831533494d60bb241e1de7d194ef1e8116719fc
Kivashan <bvkivashan1982@gmail.com>
2023-05-16 03:24:13 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,4 +8,4 @@ def no_c(my_string):
             if (my_string[i] != 'c') and (my_string[i] != 'C'):
                 new_string += my_string[i]
             i += 1
-	return (new_string)
+        return (new_string)
