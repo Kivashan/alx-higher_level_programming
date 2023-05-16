c58144349c4cc99105762d4d19e909ef0387d89e
Kivashan <bvkivashan1982@gmail.com>
2023-05-16 16:40:03 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_new = [0, 0]
+    list_a = [*tuple_a, ]
+    list_b = [*tuple_b, ]
+
+    if len(tuple_a) < 2:
+        for i in range(len(list_a), 2):
+            list_a.append(0)
+    if len(tuple_b) < 2:
+        for i in range(len(list_b), 2):
+            list_b.append(0)
+    list_new[0] = list_a[0] + list_b[0]
+    list_new[1] = list_a[1] + list_b[1]
+    return (list_new)
