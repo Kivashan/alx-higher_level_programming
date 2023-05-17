6fa08738143a48b8542937b647de17185ab2aa65
Kivashan <bvkivashan1982@gmail.com>
2023-05-16 18:04:31 -0700

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        length = len(my_list) - 1
+        if idx > 0 and idx <= length:
+            del my_list[idx]
+        return (my_list)
+    return (my_list)
