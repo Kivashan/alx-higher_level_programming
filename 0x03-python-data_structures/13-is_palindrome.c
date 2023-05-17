41bb95ef8802e7a4a21bfc3fa6e15fd43f64ab69
Kivashan <bvkivashan1982@gmail.com>
2023-05-16 19:02:20 -0700

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+int length(listint_t **head);
+
+/**
+ * is_palindrome - checks if linked list is a palindrome
+ * @head: start position of linked list
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	int i = 0, sum = 0, len;
+	listint_t *temp;
+
+	if (!head)
+		return (0);
+
+	temp = *head;
+	len = length(head);
+
+	while (i < (len / 2))
+	{
+		sum += temp->n;
+		i++;
+	}
+
+	if (len % 2 == 1)
+		i++;
+
+	while (i < len)
+	{
+		sum -= temp->n;
+		i++;
+	}
+
+	return ((sum == 0) ? 1 : 0);
+}
+
+/**
+ * length - returns length of linked list
+ * @head: the starting position of linked list
+ *
+ * Return: returns the no of elements in linked list
+ */
+int length(listint_t **head)
+{
+	int len = 0;
+	listint_t *temp = *head;
+
+	while (temp)
+	{
+		len++;
+		temp = temp->next;
+	}
+	return (len);
+}
