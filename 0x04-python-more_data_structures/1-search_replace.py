6560883e192489e08c2f04d8da6b539cc57b4ed4
Kivashan <bvkivashan1982@gmail.com>
2023-05-17 19:04:49 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list:
+        new_list = []
+        for i in my_list:
+            if i == search:
+                i = replace
+            new_list.append(i)
+        return (new_list)
+    return (my_list)
