5d8d63b5c5ed14f9222dfa382f316faff5ce3155
Kivashan <bvkivashan1982@gmail.com>
2023-05-20 02:31:28 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@
 num = 122
 
 for i in range(26):
-    print(chr(num) if num % 2 == 0 else chr(num - 32), end='')
+    print("{}".format(chr(num) if num % 2 == 0 else chr(num - 32)), end='')
     num -= 1
