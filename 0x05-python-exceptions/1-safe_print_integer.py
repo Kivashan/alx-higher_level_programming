0d46b0a929c268784a510f07156b54fc2f3b18e9
Kivashan <bvkivashan1982@gmail.com>
2023-05-29 16:57:50 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if (int(value)):
+        if (int(value) and not(float(value))):
             print("{:d}".format(value))
             return (True)
     except ValueError:
