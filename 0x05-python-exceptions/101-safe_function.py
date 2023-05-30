c90334b24aa4167ebf25f7c43ff2f5bd99db94a5
Kivashan <bvkivashan1982@gmail.com>
2023-05-30 07:58:45 -0700

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+import sys
+
 
 def safe_function(fct, *args):
     try:
         return (fct(*args))
     except (NameError, TypeError, IndexError, ValueError) as ntiv:
-        print("Exception: {}".format(ntiv))
+        print("Exception: {}".format(ntiv), file=sys.stderr)
         return (None)
     except (ZeroDivisionError) as ntiv:
-        print("Exception: {}".format(ntiv))
+        print("Exception: {}".format(ntiv), file=sys.stderr)
         return (None)
