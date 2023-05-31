6e96dc4eb7fa943b465bb6545953b7c706fb871c
Kivashan <bvkivashan1982@gmail.com>
2023-05-30 20:10:55 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''Defining a Square class'''
+
+
+class Square:
+    '''A class defining a square(Geometric shape)'''
+
+    def __init__(self, size):
+        '''Instantiating an object of the square class
+        Args:
+            param1: first parameter
+        '''
+        self.__size = size
