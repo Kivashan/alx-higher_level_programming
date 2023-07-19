29fedd5da19393ec65138d42b7904e141930f215
Kivashan <bvkivashan1982@gmail.com>
2023-07-19 04:35:46 -0700

    9-rectangle.py

--- 0x08-python-more_classes/9-main.py
+++ 0x08-python-more_classes/9-main.py
@@ -4,3 +4,5 @@ Rectangle = __import__('9-rectangle').Rectangle
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
+print(type(my_square))
+print(my_square.__dict__)
