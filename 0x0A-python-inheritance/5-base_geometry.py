76a8b510852f2329bf1331d6bcdddb0b725c4156
Kivashan <bvkivashan1982@gmail.com>
2023-07-20 07:35:41 -0700

    Task 5 and 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''A module containing a class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''A class defining a BaseGeometry object'''
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''A module containing a class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''A class defining a BaseGeometry object'''
+
+    def area(self):
+        '''Method that returns the area of the BaseGeometry object'''
+        raise Exception("area() is not implemented")
