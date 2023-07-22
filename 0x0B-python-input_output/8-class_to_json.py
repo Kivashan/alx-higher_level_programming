a17643311b52bea6038e3f669831dbd1e169147a
Kivashan <bvkivashan1982@gmail.com>
2023-07-22 08:44:05 -0700

    Removed import from code

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3i
+#!/usr/bin/python3
 '''A module containing a function that returns the dictionary description
 with simple data structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object
 '''
-import json
 
 
 def class_to_json(obj):
     '''Returns a dictionary of an objects attributes'''
 
     if "__dict__" in dir(obj):
-        obj_dict = json.dumps(obj.__dict__)
-        return json.loads(obj_dict)
+        obj_dict = obj.__dict__
+        return obj_dict
