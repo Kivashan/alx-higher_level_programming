f9215257a1eb963ad2b137539fe88fc7b5ee1c11
Kivashan <bvkivashan1982@gmail.com>
2023-07-22 10:03:04 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''A modules containing a Student class'''
+
+
+class Student:
+    '''A class defining a student object'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Constructor method for Student object'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''returns a dictionary representation of a student object'''
+        obj_dict = self.__dict__
+        new_dict = dict()
+
+        if isinstance(attrs, list):
+            for item in attrs:
+                if item in obj_dict:
+                    new_dict[item] = obj_dict[item]
+            if len(new_dict) > 0:
+                return new_dict
+            return obj_dict
+        else:
+            return obj_dict
