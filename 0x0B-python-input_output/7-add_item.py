c039c815e6a1b3c61f9765d13f9a17e161e5fbca
Kivashan <bvkivashan1982@gmail.com>
2023-07-22 06:45:30 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,18 +7,15 @@ load = __import__("6-load_from_json_file").load_from_json_file
 save = __import__("5-save_to_json_file").save_to_json_file
 
 
-list_obj = []
-
 try:
-    list_obj += load("add_item.json")
+    list_obj = load("add_item.json")
 
 except FileNotFoundError:
-    with open("add_item.json", 'w', encoding='utf-8') as f:
-        pass
+    list_obj = []
 
 finally:
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             list_obj.append(sys.argv[i])
 
-        save(list_obj, "add_item.json")
+    save(list_obj, "add_item.json")
