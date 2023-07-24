0e98f18dd70b9e265646760d2b3a1b316957199b
Kivashan <bvkivashan1982@gmail.com>
2023-07-24 15:16:19 -0700

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''Returns the json representation of list_dictionaries
 
