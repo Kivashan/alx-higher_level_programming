1da0308c240bd86208faeeed09dff7fbcb356c92
Kivashan <bvkivashan1982@gmail.com>
2023-07-24 14:12:12 -0700

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,17 @@ class Square(Rectangle):
         # calling constructor of Rectangle class
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        '''Getter method for size attribute'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''Setter method for size attribute'''
+        self.width = value
+        self.height = value
+
     def __str__(self):
         '''Returns a string representation of a Square object'''
 
