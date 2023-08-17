25feb6718b7994d5ea3835dd7b05436c7d0a6b6d
Kivashan <bvkivashan1982@gmail.com>
2023-08-16 20:05:29 -0700

    Task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked.
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows, tv_show_genres
+WHERE (tv_shows.id = tv_show_genres.show_id AND tv_show_genres.show_id IS NOT NULL)
+ORDER BY title ASC, genre_id ASC;
