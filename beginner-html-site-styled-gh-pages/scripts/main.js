0d7af63a0b35b0485a5f02188a931e309cd78cdc
Kivashan <bvkivashan1982@gmail.com>
2023-09-11 04:07:14 -0700

    update

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -4,7 +4,30 @@ myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
     myImage.setAttribute("src", "images/firefox2.png");
-  } else {
+  }
+  else {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+/* function that sets a user name */
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+/* checks if username is set if not call the setUserName function */
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
