c29718f197ecba8d68493c21ddfe4acffac79d80
Kivashan <bvkivashan1982@gmail.com>
2023-12-12 03:03:12 -0800

    Added new tasks

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+let a;
+let b;
+
+if (!argv[2]) {
+  a = b = 'undefined';
+} else if (!argv[3]) {
+  a = argv[2];
+  b = 'undefined';
+} else {
+  a = argv[2];
+  b = argv[3];
+}
+
+console.log(`${a} is ${b}`);
