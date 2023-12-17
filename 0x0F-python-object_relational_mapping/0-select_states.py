57ebd903daa491261674084850dbe00b61bbe2da
Kivashan <bvkivashan1982@gmail.com>
2023-12-16 18:10:57 -0800

    My first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """Create connection with database"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], db=argv[3])
+
+    """Create cursor object"""
+    cur = db.cursor()
+
+    """Select all states in database"""
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for i in rows:
+        print("{}".format(i))
