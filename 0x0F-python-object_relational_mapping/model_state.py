f82543e683c5a1e58d52784dbfe63481b02ce483
Kivashan <bvkivashan1982@gmail.com>
2023-12-25 17:02:44 -0800

    Task 8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """State class"""
 
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
+
 
 class State(Base):
     """A class that inherits from declarative_base in order to map our class
